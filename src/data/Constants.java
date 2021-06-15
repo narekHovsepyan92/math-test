41206173deaeca64a0e06099fafb32722ff9574b
Narek <narekHovsepyan@gmail.com>
2021-06-15 19:46:12 +0400

    Created constants

---
+++ src/data/Constants.java
@@ -0,0 +1,11 @@
+package data;
+
+public class Constants {
+
+    public static final String COMMAND_START = "start";
+    public static final String COMMAND_RESET = "reset";
+    public static final String COMMAND_RESULT = "result";
+    public static final String COMMAND_LIST = "list";
+    public static final String COMMAND_RENAME = "rename";
+
+}
