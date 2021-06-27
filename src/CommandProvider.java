732a2ac4e6f012e4cce822aebb6430a922c0b522
Narek <narekHovsepyan@gmail.com>
2021-06-27 17:16:58 +0400

    Add class ScannerProvider

--- src/CommandProvider.java
+++ src/CommandProvider.java
@@ -1,18 +1,16 @@
 import command.*;
-import data.Constants;
 
 import java.util.ArrayList;
 
 public class CommandProvider {
 
-    ListCommand listCommand = new ListCommand();
-    RenameCommand renameCommand = new RenameCommand();
-    ResetCommand resetCommand = new ResetCommand();
-    ResultCommand resultCommand = new ResultCommand();
-    StartCommand startCommand = new StartCommand();
-
     public ArrayList<Command> getCommandList() {
         ArrayList<Command> command = new ArrayList<>();
+        ListCommand listCommand = new ListCommand();
+        RenameCommand renameCommand = new RenameCommand();
+        ResetCommand resetCommand = new ResetCommand();
+        ResultCommand resultCommand = new ResultCommand();
+        StartCommand startCommand = new StartCommand();
         command.add(listCommand);
         command.add(renameCommand);
         command.add(resetCommand);
---
+++ src/ScannerProvider.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class ScannerProvider {
+
+    private Scanner getScanner() {
+        return new Scanner(System.in);
+    }
+}
